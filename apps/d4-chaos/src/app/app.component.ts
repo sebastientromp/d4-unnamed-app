25ad4b5fd0ddfd8d7392e17fb51706f6d92b4c6d
sebastientromp <sebastien.tromp@gmail.com>
2023-03-23 18:40:29 +0100

    Initial commit

---
+++ apps/d4-chaos/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'd4-chaos-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'd4-chaos';
+}
